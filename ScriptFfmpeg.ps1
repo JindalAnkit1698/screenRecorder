1f7b6f9b399363457e08dbf887ffa2df2bdc0619
JINDAL <ankit.jindal@thermofisher.com>
2023-07-08 16:41:33 +0530

    Base logic

--- ScriptFfmpeg.ps1
+++ ScriptFfmpeg.ps1
@@ -1,8 +1,25 @@
 $STARTTIME = Get-Date
 Write-Host "Script to setup FFMPEG" -ForegroundColor Blue
 
+$ffmpegPath = "C:\ffmpeg\bin"
+$targetPath = "."
+$checkPath = $env:Path -split ';' | Where-Object { $_ -like '*ffmpeg*' }
 $URL = "https://www.gyan.dev/ffmpeg/builds/packages/ffmpeg-6.0-essentials_build.zip"
 $OUTFILE = "ffmpeg.zip"
+if (-not $checkPath) {
+    if (Test-Path -Path $ffmpegPath) {
+        $env:Path = "$ffmpegPath;" + $env:Path
+    }
+    else {
+        $downloadedFile = Invoke-WebRequest -Uri $URL -OutFile $OUTFILE
+        if ($downloadedFile.StatusCode -eq 200) {
+            Expand-Archive -Path $OUTFILE
+        }
+        Expand-Archive -Path $OUTFILE -DestinationPath .
+        Remove-Item -Path $OUTFILE -Force
+        Get-ChildItem -Path $targetPath
+    }
+}
 
 Invoke-WebRequest -Uri $URL -OutFile $OUTFILE
 
