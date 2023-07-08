c74bdbe4d620ad75feeecca0dd4ea8d2d2a81d82
JINDAL <ankit.jindal@thermofisher.com>
2023-07-08 22:59:36 +0530

    Adding major fucntionalities

--- ScriptFfmpeg.ps1
+++ ScriptFfmpeg.ps1
@@ -1,33 +1,33 @@
-$STARTTIME = Get-Date
+# $STARTTIME = Get-Date
 Write-Host "Script to setup FFMPEG" -ForegroundColor Blue
 
 $ffmpegPath = "C:\ffmpeg\bin"
-$targetPath = "."
+$targetPath = "C:\ffmpeg"
 $checkPath = $env:Path -split ';' | Where-Object { $_ -like '*ffmpeg*' }
-$URL = "https://www.gyan.dev/ffmpeg/builds/packages/ffmpeg-6.0-essentials_build.zip"
-$OUTFILE = "ffmpeg.zip"
-if (-not $checkPath) {
-    if (Test-Path -Path $ffmpegPath) {
-        $env:Path = "$ffmpegPath;" + $env:Path
-    }
-    else {
-        $downloadedFile = Invoke-WebRequest -Uri $URL -OutFile $OUTFILE
-        if ($downloadedFile.StatusCode -eq 200) {
-            Expand-Archive -Path $OUTFILE
-        }
-        Expand-Archive -Path $OUTFILE -DestinationPath .
-        Remove-Item -Path $OUTFILE -Force
-        Get-ChildItem -Path $targetPath
-    }
+$url = "https://www.gyan.dev/ffmpeg/builds/packages/ffmpeg-6.0-essentials_build.zip"
+$outFile = "C:\ffmpeg.zip"
+
+# Function to set the temporary path
+function Set-TempPath {
+    $env:Path = "$ffmpegPath;" + $env:Path
 }
 
-Invoke-WebRequest -Uri $URL -OutFile $OUTFILE
-
-Expand-Archive -Path $OUTFILE
-
-Write-Host "Done" -ForegroundColor Green
-
-Write-Host "StartTime => $STARTTIME"
-$ProcessFolder = (Get-ChildItem ffmpeg).Name
-Move-Item -Path ffmpeg/$ProcessFolder/* -Destination .\ffmpeg -Force
-Remove-Item -Path .\ffmpeg/$ProcessFolder -Recurse -Force
+if (-not $checkPath) {
+    if (-not (Test-Path -Path $ffmpegPath)) {
+        Invoke-WebRequest -Uri $url -OutFile $outFile
+        Expand-Archive -Path $outFile -DestinationPath $targetPath
+        Remove-Item -Path $outFile
+        $extractedFolder = Get-ChildItem -Path $targetPath
+        Get-ChildItem -LiteralPath $extractedFolder.FullName -Recurse | Move-Item -Destination $targetPath
+        Remove-Item -LiteralPath $extractedFolder.FullName -Force
+    }
+    Set-TempPath
+}
+$userName = $env:USERNAME
+$timestamp = Get-Date -Format "yyyyMMdd-HHmmss"
+$outputFile = "C:\Users\$userName\Videos\$timestamp.mp4"
+Write-Host "Recording started to stop the recoding close the child window" -ForegroundColor Cyan
+Start-Process -FilePath ffmpeg -ArgumentList "-f gdigrab -framerate 16 -i desktop -an -c:v libx264 -preset veryfast -pix_fmt yuv420p -b:v 600k -maxrate 600k $outputFile" -Wait
+Write-Host "converting wait"
+Start-Process -FilePath ffmpeg -ArgumentList "-i $outputFile `"C:\Users\$userName\Videos\$timestamp.gif`"" -Wait -WindowStyle Hidden
+Write-Host "Done"
